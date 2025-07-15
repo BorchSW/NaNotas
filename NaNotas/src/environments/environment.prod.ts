46b0752afa2da696ea2d1c196547fea50cea6fac
Borjailz <borjailzarbe1@gmail.com>
2025-07-15 20:16:07 +0200

    Puesta en marcha
    Falta componente registros y login...

---
+++ NaNotas/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBEv1LpQRP-V4QOL7jHe128D7YDwjPWC2k",
+    authDomain: "nanotas-2fdc0.firebaseapp.com",
+    projectId: "nanotas-2fdc0",
+    storageBucket: "nanotas-2fdc0.firebasestorage.app",
+    messagingSenderId: "23249468021",
+    appId: "1:23249468021:web:acce06cc178618ebc7563c"
+  }
+};
